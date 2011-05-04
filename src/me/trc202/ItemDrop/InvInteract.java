8efb36deaff9833f9f0df355bde1c8881fd066c0
trc202 <trc202@gmail.com>
2011-05-04 16:49:44 -0500

    Now checks inventory when a Crafting Table is opened.

--- src/me/trc202/ItemDrop/InvInteract.java
+++ src/me/trc202/ItemDrop/InvInteract.java
@@ -12,16 +12,18 @@ public class InvInteract extends PlayerListener {
 	public InvInteract(ItemD instance){
 		plugin = instance;
 	}
+	@Override
 	public void onPlayerInteract(PlayerInteractEvent event){
 		Player player = event.getPlayer();
 		if(plugin.IDEnable == 1)
 		{
+			//player.sendMessage(ChatColor.GREEN + event.getEventName());
 			if(event.getAction() == Action.RIGHT_CLICK_BLOCK)  //Checks to see if player right clicked a block, otherwise does nothing
 			{
 				Block myblock = event.getClickedBlock();
-				if(myblock.getTypeId() == 54 || myblock.getTypeId() == 61 || myblock.getTypeId() == 62 || myblock.getTypeId() == 23)
+				if(myblock.getTypeId() == 54 || myblock.getTypeId() == 61 || myblock.getTypeId() == 62 || myblock.getTypeId() == 23 || myblock.getTypeId() == 58)
 				{
-					//player.sendMessage(ChatColor.RED + "You clicked a chest, furnace, or despencer!");
+					//player.sendMessage(ChatColor.RED + "You clicked a chest, furnace, despencer, or craftingtable!");
 					for (int counter = 0; counter < plugin.blockdisabled.size(); counter = counter+1) 
 					{   
 					    if(player.getInventory().contains(plugin.blockdisabled.get(counter)))
